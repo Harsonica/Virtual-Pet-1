fe6a9bb3ec1c2e53796e3fdb9e28e0e8eb1cc11f
Harsonica <72625830+Harsonica@users.noreply.github.com>
2020-12-23 22:56:02 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock, dogImage; 
+
+function preload()
+{
+  //load images here
+  dogImage = loadImage("images/dogImg.png")
+  happyDog = loadImage("images/dogImg1.png")
+}
+
+function setup() {
+  createCanvas(500, 500);
+  dog = createSprite(50, 70, 80, 90);
+  dog.addImage(dogImage)
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
+  
+}
+
+
+function draw() {  
+
+background(46,139,87)
+
+if(keyWentDown(UP_ARROW)){
+   writeStock(foodS);
+   dog.addImage(dogHappy);
+
+}
+
+
+  drawSprites();
+  //add styles here
+  text("Note: Press Up_ARROW key to feed the Drago Milk!")
+  textSize(12)
+  fill("white")
+
+}
+
+function readStock(data){
+  foodS= data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+  database.ref('/').update({
+    Food:x
+  })
+}
+
+
+
